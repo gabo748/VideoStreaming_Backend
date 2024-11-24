64a21d9497196385b5626f0060973805ad090584
Gabriel Campos <gaeducampos@gmail.com>
2024-11-24 16:27:04 -0600

    ✨ Update response

--- src/main/java/com/stream/app/controller/LoginController.java
+++ src/main/java/com/stream/app/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.stream.app.controller;
 
 import com.stream.app.model.Login;
+import com.stream.app.payload.CustomMessage;
 import com.stream.app.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,17 +19,28 @@ public class LoginController {
     private LoginService loginService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestParam String email, @RequestParam String username, @RequestParam String password) {
+    public ResponseEntity<?> registerUser(@RequestParam String email, @RequestParam String username, @RequestParam String password) {
         loginService.registerUser(email, username, password);
-        return ResponseEntity.ok("User registered successfully!");
+
+
+        CustomMessage successResponse = new CustomMessage("User registered successfully!", true);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(successResponse);
     }
 
     @PostMapping("/validate")
-    public ResponseEntity<String> validateUser(@RequestParam String username, @RequestParam String password) {
+    public ResponseEntity<?> validateUser(@RequestParam String username, @RequestParam String password) {
         Login login = loginService.findByUsername(username);
         if (login != null && loginService.validatePassword(password, login.getPassword())) {
-            return ResponseEntity.ok("Password is valid!");
+            CustomMessage successResponse = new CustomMessage("User Login in successfully!", true);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(successResponse);
         }
-        return ResponseEntity.status(401).body("Invalid username or password!");
+
+        CustomMessage failureResponse = new CustomMessage("Credentials Invalid", false);
+        return ResponseEntity.status(401).body(failureResponse);
     }
 }
