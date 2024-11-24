a5f8baa90aada24b91ea5e931420d8d0e6e00f41
Gabriel Campos <gaeducampos@gmail.com>
2024-11-24 16:59:07 -0600

    ✨ Add Login and allow cors

--- src/main/java/com/stream/app/configuration/SecurityConfig.java
+++ src/main/java/com/stream/app/configuration/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -16,6 +22,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // Proper syntax for disabling CSRF
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/login/**").permitAll() // Allow access to login endpoints
+                        .requestMatchers("/api/v1/videos/**").permitAll()
                         .anyRequest().authenticated() // Protect all other endpoints
                 );
         return http.build();
@@ -24,5 +31,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // Bean for password encoding
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Allow all origins
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow all HTTP methods
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Allow all headers
+        configuration.setAllowCredentials(true); // Allow credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply to all endpoints
+        return source;
     }
 }
